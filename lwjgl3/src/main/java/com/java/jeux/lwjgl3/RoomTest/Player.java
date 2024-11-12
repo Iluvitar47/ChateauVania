4fd0e14d1eb49b7ab17c037932052253280434a7
Iluvitar47 <72810716+Iluvitar47@users.noreply.github.com>
2024-11-12 15:43:15 +0100

    [REFINE] Enhance Player class by integrating Pixmap handling and optimizing sprite dimensions calculation

--- lwjgl3/src/main/java/com/java/jeux/lwjgl3/RoomTest/Player.java
+++ lwjgl3/src/main/java/com/java/jeux/lwjgl3/RoomTest/Player.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.graphics.Pixmap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +19,7 @@ public class Player extends Character {
     private final float attackDuration = 0.5f;
     public boolean facingRight = true, isWalking = false, isAttacking = false;
     private SpriteResourceManager spriteManager;
+    private Pixmap currentPixmap;
 
     public Player(float startX, float startY) {
         super(startX, startY);
@@ -28,25 +29,49 @@ public class Player extends Character {
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+
         String directory = "assets/Characters/Gorgon_1";
         Map<String, Integer> animations = new HashMap<>();
         animations.put("Idle", 7);
         animations.put("Walk", 13);
         animations.put("Attack", 10);
+
+
         spriteManager.loadSprites(directory, animations, "single");
+
+
         idleAnimation = new Animation<>(0.1f, spriteManager.getAnimation(directory, "Idle"));
         walkAnimation = new Animation<>(0.1f, spriteManager.getAnimation(directory, "Walk"));
         attackAnimation = new Animation<>(0.1f, spriteManager.getAnimation(directory, "Attack"));
         currentAnimation = idleAnimation;
+
+
+        currentPixmap = preparePixmap();
+        float[] dimensions = spriteManager.calculateFrameDimensions(idleAnimation.getKeyFrame(0), currentPixmap);
+        spriteWidth = dimensions[0];
+        spriteHeight = dimensions[1];
+        hitboxOffsetX = dimensions[2];
+
+
+    }
+
+    private Pixmap preparePixmap() {
+        TextureRegion firstFrame = idleAnimation.getKeyFrame(0);
+        firstFrame.getTexture().getTextureData().prepare();
+        return firstFrame.getTexture().getTextureData().consumePixmap();
     }
 
     public void render(SpriteBatch batch) {
+
         TextureRegion currentFrame = currentAnimation.getKeyFrame(elapsedTime, true);
+
         if (facingRight) {
             batch.draw(currentFrame, position.x, position.y, currentFrame.getRegionWidth(), currentFrame.getRegionHeight());
         } else {
             batch.draw(currentFrame, position.x + currentFrame.getRegionWidth(), position.y, -currentFrame.getRegionWidth(), currentFrame.getRegionHeight());
         }
+
     }
 
     public void update(float deltaTime) {
@@ -65,9 +90,11 @@ public class Player extends Character {
         } else {
             newAnimation = idleAnimation;
         }
+
         if (newAnimation != currentAnimation) {
             currentAnimation = newAnimation;
         }
+
         handleMovement(deltaTime);
     }
 
@@ -99,12 +126,12 @@ public class Player extends Character {
 
     @Override
     public float getWidth() {
-        return currentAnimation.getKeyFrame(0).getRegionWidth();
+        return spriteWidth;
     }
 
     @Override
     public float getHeight() {
-        return currentAnimation.getKeyFrame(0).getRegionHeight();
+        return spriteHeight;
     }
 
     @Override
@@ -116,6 +143,9 @@ public class Player extends Character {
     public void dispose() {
         batch.dispose();
         spriteManager.dispose();
+        if (currentPixmap != null) {
+            currentPixmap.dispose();
+        }
     }
 
     public float getX() {
