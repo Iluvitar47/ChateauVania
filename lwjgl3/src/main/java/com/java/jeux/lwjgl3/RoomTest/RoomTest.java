6016db5cdc068e70d944d4996de72e6abb059308
Iluvitar47 <72810716+Iluvitar47@users.noreply.github.com>
2024-11-12 11:54:27 +0100

    [ADD] New betatest level : "RoomTest"

---
+++ lwjgl3/src/main/java/com/java/jeux/lwjgl3/RoomTest/RoomTest.java
@@ -0,0 +1,101 @@
+package com.java.jeux.lwjgl3.RoomTest;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
+public class RoomTest extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Player player;
+    private TestEnemie enemy;
+    private RoomTestMapLoad roomTestMapLoad;
+    private ShapeRenderer shapeRenderer;
+    private GravityTest gravityTest;
+
+    @Override
+    public void create() {
+
+        roomTestMapLoad = new RoomTestMapLoad();
+        roomTestMapLoad.create();
+
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+
+
+        player = new Player(200, 36);
+        player.create();
+
+        enemy = new TestEnemie(800, 800);
+        enemy.create();
+
+        gravityTest = new GravityTest(roomTestMapLoad.getGroundObjects());
+    }
+
+    @Override
+    public void render() {
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        gravityTest.applyGravity(player, deltaTime);
+        gravityTest.applyGravity(enemy, deltaTime);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        roomTestMapLoad.render();
+
+
+        shapeRenderer.setProjectionMatrix(roomTestMapLoad.getCamera().combined);
+
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.RED);
+        for (Rectangle ground : roomTestMapLoad.getGroundObjects()) {
+            shapeRenderer.rect(ground.x, ground.y, ground.width, ground.height);
+        }
+        shapeRenderer.end();
+
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.YELLOW);
+        Rectangle gravityHitbox = player.getGravityHitbox();
+        shapeRenderer.rect(gravityHitbox.x, gravityHitbox.y, gravityHitbox.width, gravityHitbox.height);
+        shapeRenderer.end();
+
+
+
+        player.update(deltaTime);
+        enemy.update(deltaTime);
+        if (!enemy.isDead()) {
+            checkCollision();
+        }
+
+
+        batch.begin();
+        player.render(batch);
+        enemy.render(batch);
+        batch.end();
+    }
+
+        private void checkCollision() {
+        Rectangle playerBounds = player.getBounds();
+        Rectangle enemyBounds = enemy.getBounds();
+
+
+        if (player.isAttacking() && playerBounds.overlaps(enemyBounds)) {
+            enemy.die();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        roomTestMapLoad.dispose();
+        batch.dispose();
+        shapeRenderer.dispose();
+        player.dispose();
+        enemy.dispose();
+    }
+}
---
+++ lwjgl3/src/main/java/com/java/jeux/lwjgl3/RoomTest/RoomTestMapLoad.java
@@ -0,0 +1,94 @@
+package com.java.jeux.lwjgl3.RoomTest;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomTestMapLoad {
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
+    private ShapeRenderer shapeRenderer;
+
+    public void create() {
+        map = new TmxMapLoader().load("Maps/RoomTest.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        // Initialisation du ShapeRenderer
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    public void render() {
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        // Dessine les rectangles du calque "Sol" en rouge pour le débogage
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.RED);
+
+        for (Rectangle rect : getGroundObjects()) {
+            shapeRenderer.rect(rect.x, rect.y, rect.width, rect.height);
+        }
+
+        shapeRenderer.end();
+    }
+
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
+
+    public void dispose() {
+        map.dispose();
+        mapRenderer.dispose();
+        shapeRenderer.dispose(); // Libère les ressources du ShapeRenderer
+    }
+
+    public int getMapWidth() {
+        int width = map.getProperties().get("width", Integer.class);
+        int tileWidth = map.getProperties().get("tilewidth", Integer.class);
+        return width * tileWidth;
+    }
+
+    public int getMapHeight() {
+        int height = map.getProperties().get("height", Integer.class);
+        int tileHeight = map.getProperties().get("tileheight", Integer.class);
+        return height * tileHeight;
+    }
+
+    public List<Rectangle> getGroundObjects() {
+        List<Rectangle> groundObjects = new ArrayList<>();
+        if (map.getLayers().get("platform") != null) {
+            MapObjects objects = map.getLayers().get("platform").getObjects();
+            for (MapObject object : objects) {
+                if (object instanceof PolygonMapObject) {
+                    Polygon polygon = ((PolygonMapObject) object).getPolygon();
+                    Rectangle boundingRectangle = polygon.getBoundingRectangle();
+                    groundObjects.add(boundingRectangle);
+                }
+            }
+        } else {
+            Gdx.app.log("MapTest", "Le calque 'Sol' est introuvable.");
+        }
+        return groundObjects;
+    }
+}
